c9b6203a9ba80279f899dcbb328811c775839094
kennyongkojoyo <37732703+kennyongkojoyo@users.noreply.github.com>
2018-11-13 20:44:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+/*  EXPRESS SETUP  */
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("You have successfully logged in"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  FACEBOOK AUTH  */
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+const FACEBOOK_APP_ID = '2243086945949265';
+const FACEBOOK_APP_SECRET = '0f6ead5631eedf47005bb412172fbd13';
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
